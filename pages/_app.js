dc4678720a9da8fd6371f04b03a8cbe6e75b93ae
Gabriel Garino <gabriel@Gabriels-MacBook-Air.local>
2025-05-25 20:58:00 -0500

    User Email auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { supabase } from "@/lib/supabaseClient";
 import "@/styles/globals.css";
 import Head from "next/head";
@@ -5,6 +7,15 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.location.hash.includes("access_token")) {
+      supabase.auth.getSessionFromUrl().then(() => {
+        router.replace("/");
+      });
+    }
+  }, []);
   return (
     <>
       <Head>
